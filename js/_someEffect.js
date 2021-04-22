bc2eeefeb5b0d6eba7e15462691d922152770dee
Macgia99 <macgia1999@gmail.com>
2021-04-22 12:08:17 +0700

    Update manything

---
+++ js/_someEffect.js
@@ -0,0 +1,26 @@
+$(function () {
+  var typed = new Typed("#typed", {
+    strings: [
+      "你好!",
+      "Hello!",
+      "Saluton!",
+      "Hallo!",
+      "Bonjour!",
+      " Buongiorno!",
+      "こんにちは!",
+      " 안영하세요!",
+      " Сайн уу!",
+      "Olá!",
+      "Привет!",
+      "¡Hola!",
+      "สวัสดีคะ!",
+      "Swazdo-lah!",
+      "Chào mừng!",
+    ],
+    typeSpeed: 70,
+    shuffle: true,
+    showCursor: false,
+    loop: true,
+    backDelay: 1500,
+  });
+});
---
+++ js/contract.js
@@ -0,0 +1,18 @@
+$(function () {
+  $.getJSON("./data/contact.json", function (data) {
+    console.log(data);
+    $.each(data, function (index, value) {
+      $("#contactCon").append(
+        '<li><a href="' +
+          value.link +
+          '"><i id="contactIco" class="' +
+          value.icon +
+          '"></i><span>' +
+          value.name +
+          "</span></a></li>"
+      );
+    });
+  }).fail(function () {
+    console.log(" Not get anything");
+  });
+});
